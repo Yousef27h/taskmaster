b535d86ccf258913cba6ca14d5d65badd0fe90c5
Yousef <yousef_salem97@yahoo.com>
2021-08-16 22:07:01 +0300

    adding espresso test

--- app/src/main/java/com/example/taskmaster/TaskDetailsActivity.java
+++ app/src/main/java/com/example/taskmaster/TaskDetailsActivity.java
@@ -1,5 +1,6 @@
 package com.example.taskmaster;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.TextView;
@@ -8,6 +9,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class TaskDetailsActivity extends AppCompatActivity {
 
+    @SuppressLint("SetTextI18n")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -15,7 +17,12 @@ public class TaskDetailsActivity extends AppCompatActivity {
 
         Intent intent = getIntent();
 
-        TextView textView = (TextView) findViewById(R.id.textView6);
-        textView.setText(intent.getExtras().getString("task_name"));
+        TextView textTitle = (TextView) findViewById(R.id.textTitle);
+        TextView textState = (TextView) findViewById(R.id.textState);
+        TextView textDescription = (TextView) findViewById(R.id.textDescription);
+
+        textTitle.setText(intent.getExtras().getString("task_name"));
+        textState.setText("State: " + intent.getExtras().getString("task_state"));
+        textDescription.setText("Description: " + intent.getExtras().getString("task_description"));
     }
 }
